5a7cbdfd6494ce2f446fa58127fdf6f180e17015
pwsd21 <pawansachdeva1998@gmail.com>
2023-12-25 21:37:41 +0530

    Get API routes created

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const chats = require("./data/data");
+const dotenv = require("dotenv");
 
 const app = express();
 
-app.get("/", (req, res) => res.end("API is running..."));
+app.get("/", (req, res) => res.send("API is running..."));
 
-app.listen(5000, console.log("Server started at PORT 5000..."));
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((el) => el._id === req.params.id);
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started at PORT ${PORT}...`));
