2e82a320ba5ec94f68a2e55cf8d66aae80083500
pwsd21 <pawansachdeva1998@gmail.com>
2023-12-31 15:21:45 +0530

    rename group route and controller created for specific user

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -5,7 +5,7 @@ const {
   createGroupChat,
   //   removeFromGroup,
   //   addToGroup,
-  //   renameGroup,
+  renameGroup,
 } = require("../controllers/chatController");
 const { protect } = require("../middleware/authMiddleware");
 
@@ -14,7 +14,7 @@ const router = express.Router();
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
 router.route("/group").post(protect, createGroupChat);
-// router.route("/rename").put(protect, renameGroup);
+router.route("/rename").put(protect, renameGroup);
 // router.route("/groupremove").put(protect, removeFromGroup);
 // router.route("/groupadd").put(protect, addToGroup);
 
