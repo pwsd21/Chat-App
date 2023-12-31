53814594bc48dd102093b21108b1ed00615404fa
pwsd21 <pawansachdeva1998@gmail.com>
2023-12-31 15:10:50 +0530

    Fetch chats route and controller created for specific user

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const {
   accessChat,
-  //   fetchChats,
+  fetchChats,
   //   createGroupChat,
   //   removeFromGroup,
   //   addToGroup,
@@ -12,7 +12,7 @@ const { protect } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
-// router.route("/").get(protect, fetchChats);
+router.route("/").get(protect, fetchChats);
 // router.route("/group").post(protect, createGroupChat);
 // router.route("/rename").put(protect, renameGroup);
 // router.route("/groupremove").put(protect, removeFromGroup);
