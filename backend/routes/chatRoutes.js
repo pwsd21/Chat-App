bb4d5d3634e88adc51dc619d446a97c8a6473feb
pwsd21 <pawansachdeva1998@gmail.com>
2023-12-31 15:17:56 +0530

    create group chat route and controller created for specific user

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const {
   accessChat,
   fetchChats,
-  //   createGroupChat,
+  createGroupChat,
   //   removeFromGroup,
   //   addToGroup,
   //   renameGroup,
@@ -13,7 +13,7 @@ const router = express.Router();
 
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
-// router.route("/group").post(protect, createGroupChat);
+router.route("/group").post(protect, createGroupChat);
 // router.route("/rename").put(protect, renameGroup);
 // router.route("/groupremove").put(protect, removeFromGroup);
 // router.route("/groupadd").put(protect, addToGroup);
