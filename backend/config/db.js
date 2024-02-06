35e6814ef2908b36bf788e89dd0f1eb33eb98afc
pwsd21 <pawansachdeva1998@gmail.com>
2024-02-06 08:59:49 +0530

    MongoDB connectivity warning fixed

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,8 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindandModify: false,
     });
 
     console.log(`MongoDB Connected:🎯 ${conn.connection.host}`.cyan.underline);
